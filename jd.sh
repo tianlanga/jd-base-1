1beb8d69a77b9490e40233f1b300b58e4c60f1e8
tianlanga <56530185+tianlanga@users.noreply.github.com>
2021-02-10 13:59:30 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -113,6 +113,7 @@ function Combin_All {
   export JDSXSY_SHARECODES=$(Combin_Sub ForOtherImmortal "")
   export JDSGMH_SHARECODES=$(Combin_Sub ForOtherSgmh "")
   export JSMOBILEFESTIVAL_SHARECODES=$(Combin_Sub ForOtherJdMobileFestival)
+  export JDGLOBAL_SHARECODES=$(Combin_Sub ForOtherGlobal)
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
